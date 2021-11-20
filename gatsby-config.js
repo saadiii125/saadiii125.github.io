c76634e058b98d9e09e08ce5e28db3ad10859a34
Danyal Ali Asghar <danyal.aliasghar@gmail.com>
2021-11-21 00:38:16 +0500

    Commit 21/11/21

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,7 @@
 const config = require('./src/config');
 
 module.exports = {
+  pathPrefix: "/danyalaliasghar.github.io",
   siteMetadata: {
     title: config.siteTitle,
     siteUrl: config.siteUrl,
