d72f478b61106eff2c1a5aa201a8cd5a095405a1
Danyal Ali Asghar <danyal.aliasghar@gmail.com>
2021-11-21 14:49:03 +0500

    Changes 21/11/21

--- src/templates/post.js
+++ src/templates/post.js
@@ -43,7 +43,7 @@ const PostTemplate = ({ data, location }) => {
     <Layout location={location}>
       <Helmet>
         <title>{title} | Chandrika Deb</title>
-        <link rel="canonical" href="https://chandrikadeb7.github.io/pensieve" />
+        <link rel="canonical" href="https://danyalaliasghar.github.io/pensieve" />
       </Helmet>
 
       <StyledPostContainer>
